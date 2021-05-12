66e02d0bb2062d999326d029e2260d79e5128f0b
Tense35 <45320484+Tense35@users.noreply.github.com>
2021-05-12 00:11:07 -0500

    Dashboard base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
+// Terceros
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Propios
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = 
+[
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: 
+    [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NopagefoundComponent  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: 
+  [
+    RouterModule.forRoot( routes )
+  ],
+  exports: 
+  [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
